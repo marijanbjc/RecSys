209468e46aac96e5aae4910524e3612b69ea8048
Maryan.Byelich <Maryan.Byelich@x5.ru>
2026-07-25 13:09:09 +0300

    Created shared-package with config, logger, models and watched_filter

---
+++ shared/config.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel, HttpUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class RedisSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="REDIS_")
+    HOST: str
+    PORT: int = 6379
+    DB: int = 0
+    INTERACTION_PREFIX: str = "interaction"
+    ALS_RECOMMENDATION_PREFIX: str = "als_recommendation"
+    TOP_RECOMMENDATION_PREFIX: str = "top_recommendation"
+
+
+class RabbitSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="RABBIT_")
+    QUEUE_NAME: str = "user_interactions"
+    ROUTING_KEY: str = "user.interact.message"
+    EXCHANGE: str = "user.interact"
+    USER: str
+    HOST: str
+    PORT: int = 5672
+    URL: str = "amqp://guest:guest@rabbitmq:5672/"
+
+
+class WatchedFilterSettings(BaseModel):
+    model_config = SettingsConfigDict(env_prefix="WF_")
+    PREFIX: str = "watched"
+
+
+class RecommendationSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="RECOMMENDATION_")
+    LOG_LEVEL: str = "INFO"
+    EPSILON: float = 0.05
+    TOP_K: int = 10
+
+
+class AppSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env')
+    redis_settings: RedisSettings = RedisSettings()
+    rabbit_settings: RabbitSettings = RabbitSettings()
+    watched_filter_settings: WatchedFilterSettings = WatchedFilterSettings()
+    recommendation_settings: RecommendationSettings = RecommendationSettings()
+
+
+settings = AppSettings()
---
+++ shared/watched_filter.py
@@ -0,0 +1,42 @@
+import redis
+
+from shared.config import AppSettings
+
+
+class WatchedFilter:
+    def __init__(self, settings: AppSettings) -> None:
+        self.watched_prefix = settings.watched_filter_settings.PREFIX
+        self.redis_connection = redis.Redis(
+            host=settings.redis_settings.HOST,
+            port=settings.redis_settings.PORT,
+            db=settings.redis_settings.DB
+        )
+
+    def add(self, user_id: str, item_ids: list[str]) -> None:
+        try:
+            for item_id in item_ids:
+                self.redis_connection.set(f"{self.watched_prefix}-{user_id}-{item_id}", 1)
+        except redis.exceptions.ConnectionError:
+            # ignore errors if redis unavailable
+            pass
+
+    def get(self, user_id: str, item_id: str) -> int | None:
+        flg = self.redis_connection.get(f"{self.watched_prefix}-{user_id}-{item_id}")
+        if flg is None:
+            return None
+        return flg.decode()
+
+    def filter_user_items(self, user_id: str, item_ids: list[str]) -> list[str]:
+        filtered_item_ids = []
+        for item_id in item_ids:
+            if self.get(user_id, item_id) is None:
+                filtered_item_ids.append(item_id)
+        return filtered_item_ids
+
+
+    def remove_all(self) -> None:
+        try:
+            self.redis_connection.delete(f"{self.watched_prefix}-*")
+        except redis.exceptions.ConnectionError:
+            # ignore errors if redis unavailable
+            pass
